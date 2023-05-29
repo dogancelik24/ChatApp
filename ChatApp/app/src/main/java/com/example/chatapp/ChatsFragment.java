b28c696cfaddfdfbd9e3d84e7a5cbabb9c64980d
dogancelik24 <116541193+dogancelik24@users.noreply.github.com>
2023-05-30 02:41:34 +0300

    ChatApp
    Final projesi

---
+++ ChatApp/app/src/main/java/com/example/chatapp/ChatsFragment.java
@@ -0,0 +1,91 @@
+package com.example.chatapp;
+
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.V;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link ChatsFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ChatsFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private RecyclerView mRecyclerView;
+    private EditText editMesaj;
+
+
+
+
+
+    public ChatsFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment ChatsFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static ChatsFragment newInstance(String param1, String param2) {
+        ChatsFragment fragment = new ChatsFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+        private void init(){
+
+            mRecyclerView = (RecyclerView) view.findViewById(R.id.chat_activity_recyclerview);
+         editMesaj = (EditText) view.findViewById (R.id.chat_activity_editmesaj);
+
+        }
+
+        @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+
+        }
+
+        public void btnMesajGonder(View V){
+
+            }
+
+
+        }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_chats, container, false);
+    }
+}
